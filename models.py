0445282025107e29b782e91a29debf4ab78e77c5
ilushka-ganushkin005 <ilushka.ganushkin005@yandex.ru>
2026-05-13 19:37:56 +0300

    retry

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
